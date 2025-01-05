22e32496d118a59b599e4768182974ded289078f
Venkataratnam Mandangi <venkataratnammandangi123@gmail.com>
2025-01-05 17:45:48 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'footer',component:FooterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
